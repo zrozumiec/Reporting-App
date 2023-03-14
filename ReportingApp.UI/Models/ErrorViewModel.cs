65236c688c51b7df2ddc5d43dc80decd54082a7b
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-03-14 13:49:20 +0100

    Initial commit

---
+++ ReportingApp.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReportingApp.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
