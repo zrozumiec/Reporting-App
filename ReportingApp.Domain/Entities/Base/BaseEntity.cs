55b3ed6d5f9048c12f0def73c44f164ab5252b98
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-03-27 19:09:41 +0200

    Add documentation to class

--- ReportingApp.Domain/Entities/Base/BaseEntity.cs
+++ ReportingApp.Domain/Entities/Base/BaseEntity.cs
@@ -1,7 +1,13 @@
 ﻿namespace ReportingApp.Domain.Entities.Base
 {
+    /// <summary>
+    /// Base entity class.
+    /// </summary>
     public abstract class BaseEntity
     {
+        /// <summary>
+        /// Gets or sets entity id.
+        /// </summary>
         public int Id { get; set; }
     }
 }
