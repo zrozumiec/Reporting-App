50401566c45d3664dbd48e8d27c9d010d10d5d22
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-04-03 17:56:05 +0200

    Add tests for location queries

---
+++ ReportingApp.Tests/CQRS/Queries/Location/GetAllLocationsQueryHandlerTest.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Moq;
+using ReportingApp.Application.CQRS.Queries.Location.GetAllLocation;
+using ReportingApp.Application.DTO;
+using ReportingApp.Domain.Entities;
+using ReportingApp.Domain.Interfaces;
+
+namespace ReportingApp.Tests.CQRS.Queries.Location
+{
+    public class GetAllLocationsQueryHandlerTest
+    {
+        [Test]
+        public async Task GetAllLocationsQuery_Should_Return_Correct_Number_Of_Locations()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IFailureLocationRepository>();
+            repositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(GetTestLocations());
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(x => x.Map<ICollection<FailureLocationDto>>(It.IsAny<ICollection<FailureLocation>>())).Returns(GetTestLocationDtos());
+            var handler = new GetAllLocationsQueryHandler(repositoryMock.Object, mapperMock.Object);
+            var query = new GetAllLocationsQuery();
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.That(GetTestLocations(), Has.Count.EqualTo(result.Count));
+            repositoryMock.Verify(x => x.GetAllAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task GetAllLocationsQuery_Should_Return_Correct_LocationDtos()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IFailureLocationRepository>();
+            repositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(GetTestLocations());
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(x => x.Map<ICollection<FailureLocationDto>>(It.IsAny<ICollection<FailureLocation>>())).Returns(GetTestLocationDtos());
+            var handler = new GetAllLocationsQueryHandler(repositoryMock.Object, mapperMock.Object);
+            var query = new GetAllLocationsQuery();
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(GetTestLocations().First().Id, Is.EqualTo(result.First().Id));
+                Assert.That(GetTestLocations().First().Street, Is.EqualTo(result.First().Street));
+                Assert.That(GetTestLocations().Last().Id, Is.EqualTo(result.Last().Id));
+                Assert.That(GetTestLocations().Last().Street, Is.EqualTo(result.Last().Street));
+            });
+            repositoryMock.Verify(x => x.GetAllAsync(), Times.Once);
+        }
+
+        private static ICollection<FailureLocation> GetTestLocations()
+        {
+            return new List<FailureLocation>
+            {
+                new FailureLocation { Id = 1, Street = "Test Location 1" },
+                new FailureLocation { Id = 2, Street = "Test Location 2" }
+            };
+        }
+
+        private static ICollection<FailureLocationDto> GetTestLocationDtos()
+        {
+            return new List<FailureLocationDto>
+            {
+                new FailureLocationDto { Id = 1, Street = "Test Location 1" },
+                new FailureLocationDto { Id = 2, Street = "Test Location 2" }
+            };
+        }
+    }
+}
---
+++ ReportingApp.Tests/CQRS/Queries/Location/GetLocationByIdQueryHandlerTest.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Moq;
+using ReportingApp.Application.CQRS.Queries.Location.GetLocationById;
+using ReportingApp.Application.DTO;
+using ReportingApp.Domain.Entities;
+using ReportingApp.Domain.Interfaces;
+
+namespace ReportingApp.Tests.CQRS.Queries.Location
+{
+    public class GetLocationByIdQueryHandlerTest
+    {
+        [Test]
+        public async Task GetLocationByIdQuery_Returns_FailureLocationDto()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IFailureLocationRepository>();
+            var mapperMock = new Mock<IMapper>();
+            var query = new GetLocationQuery(1);
+            var expectedResult = new FailureLocationDto();
+            repositoryMock.Setup(x => x.GetByIdAsync(query.LocationId)).ReturnsAsync(new FailureLocation());
+            mapperMock.Setup(x => x.Map<FailureLocationDto>(It.IsAny<FailureLocation>())).Returns(expectedResult);
+            var handler = new GetLocationQueryHandler(repositoryMock.Object, mapperMock.Object);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+    }
+}
