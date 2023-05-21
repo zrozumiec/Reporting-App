793e2871e2fb529453404e2cb152679c5ff731ed
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 18:09:05 +0200

    Add method AcceptSolutionAsync

--- ReportingApp.Domain/Interfaces/IFailureSolutionRepository.cs
+++ ReportingApp.Domain/Interfaces/IFailureSolutionRepository.cs
@@ -8,5 +8,12 @@ namespace ReportingApp.Domain.Interfaces
     /// </summary>
     public interface IFailureSolutionRepository : IBaseRepository<FailureSolution>
     {
+        /// <summary>
+        /// Async method to set solution as accepted.
+        /// </summary>
+        /// <param name="id">Solution id.</param>
+        /// <returns>Accepted solution id.</returns>
+        /// <exception cref="ArgumentException">Throws when solution with given id does not exist in database.</exception>
+        public Task<int> AcceptSolutionAsync(int id);
     }
 }
