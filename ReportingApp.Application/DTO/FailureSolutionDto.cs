2eb1741af6febb3dd063c8e513711329341b86db
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 18:22:47 +0200

    Add UserId and FailureId properties

--- ReportingApp.Application/DTO/FailureSolutionDto.cs
+++ ReportingApp.Application/DTO/FailureSolutionDto.cs
@@ -28,13 +28,23 @@ namespace ReportingApp.Application.DTO
         public bool Accepted { get; set; }
 
         /// <summary>
+        /// Gets or sets solution user id.
+        /// </summary>
+        public string UserId { get; set; } = string.Empty;
+
+        /// <summary>
         /// Gets or sets solution user.
         /// </summary>
-        public ApplicationUserDto User { get; set; } = new ();
+        public ApplicationUserDto User { get; set; } = new ApplicationUserDto();
+
+        /// <summary>
+        /// Gets or sets solution failure id.
+        /// </summary>
+        public int FailureId { get; set; }
 
         /// <summary>
         /// Gets or sets solution failure.
         /// </summary>
-        public FailureDto Failure { get; set; } = new ();
+        public FailureDto Failure { get; set; } = new FailureDto();
     }
 }
