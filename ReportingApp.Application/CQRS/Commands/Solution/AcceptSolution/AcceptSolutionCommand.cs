078f4d9a40ebdc59ffa181abed2faf9e192f7173
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 18:26:49 +0200

    Add FailureSolution CQRS Commands

---
+++ ReportingApp.Application/CQRS/Commands/Solution/AcceptSolution/AcceptSolutionCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace ReportingApp.Application.CQRS.Commands.Solution.AcceptSolution
+{
+    public class AcceptSolutionCommand : IRequest<int>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AcceptSolutionCommand"/> class.
+        /// </summary>
+        /// <param name="solutionId">Solution id.</param>
+        public AcceptSolutionCommand(int solutionId)
+        {
+            this.SolutionId = solutionId;
+        }
+
+        /// <summary>
+        /// Gets solution id.
+        /// </summary>
+        public int SolutionId { get; }
+    }
+}
---
+++ ReportingApp.Application/CQRS/Commands/Solution/AcceptSolution/AcceptSolutionCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using ReportingApp.Domain.Interfaces;
+
+namespace ReportingApp.Application.CQRS.Commands.Solution.AcceptSolution
+{
+    public class AcceptSolutionCommandHandler : IRequestHandler<AcceptSolutionCommand, int>
+    {
+        private readonly IFailureSolutionRepository repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AcceptSolutionCommandHandler"/> class.
+        /// </summary>
+        /// <param name="repository">Failure solution repository.</param>
+        public AcceptSolutionCommandHandler(IFailureSolutionRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<int> Handle(AcceptSolutionCommand request, CancellationToken cancellationToken)
+        {
+            return await this.repository.AcceptSolutionAsync(request.SolutionId);
+        }
+    }
+}
