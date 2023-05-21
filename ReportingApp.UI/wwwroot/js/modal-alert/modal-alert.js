e7ef8e275014b5bda032f19040a06d15a9021c0c
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 19:06:10 +0200

    Add js script to use modal alerts

---
+++ ReportingApp.UI/wwwroot/js/modal-alert/modal-alert.js
@@ -0,0 +1,23 @@
+let solutionIdString = "0";
+
+$(document).on("click", ".open-AddSolutionDialog", function () {
+    solutionIdString = $(this).data('id');
+    $(".modal-body #solutionId").val(solutionIdString);
+    $('#myVar').val(solutionIdString);
+});
+
+
+function acceptSolution(p1) {
+    let solutionId = Number(solutionIdString);
+    let failureId = Number(p1)
+
+    $.ajax({
+        url: '/Failure/AcceptSolution',
+        type: 'POST',
+        data: { 'solutionId': solutionId, 'failureId': failureId },
+        async: true,
+        success: function (data) {
+            location.reload(true);
+        }
+    });
+}
