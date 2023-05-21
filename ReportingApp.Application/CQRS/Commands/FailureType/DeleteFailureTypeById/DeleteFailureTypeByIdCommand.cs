35f1fc8861b49a32b516ddedb36ece21a70774cc
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 18:27:13 +0200

    Add FailureType CQRS Commands

---
+++ ReportingApp.Application/CQRS/Commands/FailureType/DeleteFailureTypeById/DeleteFailureTypeByIdCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace ReportingApp.Application.CQRS.Commands.FailureType.GetFailureTypeById
+{
+    public class DeleteFailureTypeByIdCommand : IRequest<int>
+    {
+        public DeleteFailureTypeByIdCommand(int failureTypeId)
+        {
+            FailureTypeId = failureTypeId;
+        }
+
+        public int FailureTypeId { get; }
+    }
+}
---
+++ ReportingApp.Application/CQRS/Commands/FailureType/DeleteFailureTypeById/DeleteFailureTypeByIdCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using ReportingApp.Domain.Interfaces;
+
+namespace ReportingApp.Application.CQRS.Commands.FailureType.GetFailureTypeById
+{
+    public class DeleteFailureTypeByIdCommandHandler : IRequestHandler<DeleteFailureTypeByIdCommand, int>
+    {
+        private readonly IFailureTypeRepository repository;
+
+        public DeleteFailureTypeByIdCommandHandler(IFailureTypeRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<int> Handle(DeleteFailureTypeByIdCommand request, CancellationToken cancellationToken)
+        {
+            return await this.repository.DeleteAsync(request.FailureTypeId);
+        }
+    }
+}
