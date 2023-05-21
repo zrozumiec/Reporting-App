10cea763216cafc3acfbe48b9e455cbd3c70598f
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 18:08:45 +0200

    Add method GetAllUserFailuresAsync

--- ReportingApp.Domain/Interfaces/IFailureRepository.cs
+++ ReportingApp.Domain/Interfaces/IFailureRepository.cs
@@ -8,5 +8,15 @@ namespace ReportingApp.Domain.Interfaces
     /// </summary>
     public interface IFailureRepository : IBaseRepository<Failure>
     {
+        public Task<ICollection<Failure>> GetAllUserFailuresAsync(string userId);
+
+        /// <summary>
+        /// Async method to edit failure status.
+        /// </summary>
+        /// <param name="failureId">Failure id.</param>
+        /// <param name="statusId">Status id.</param>
+        /// <returns>Edited failure id.</returns>
+        /// <exception cref="ArgumentException">Throws when failure does not exist.</exception>
+        public Task<int> EditStatusAsync(int failureId, int statusId);
     }
 }
