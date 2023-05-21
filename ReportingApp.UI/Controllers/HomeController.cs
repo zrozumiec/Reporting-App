f17c44f1847ffa9f3f0380a921388dd84c322a19
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 19:10:04 +0200

    Refactor

--- ReportingApp.UI/Controllers/HomeController.cs
+++ ReportingApp.UI/Controllers/HomeController.cs
@@ -1,32 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ReportingApp.Models;
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ReportingApp.UI.Models;
 
 namespace ReportingApp.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return this.View();
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return this.View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
