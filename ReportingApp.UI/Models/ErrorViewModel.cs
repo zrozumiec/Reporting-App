3dbd91c4fdad297dafcfa7f71838241330eea152
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 19:10:59 +0200

    Refactor

--- ReportingApp.UI/Models/ErrorViewModel.cs
+++ ReportingApp.UI/Models/ErrorViewModel.cs
@@ -4,6 +4,6 @@ namespace ReportingApp.UI.Models
     {
         public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
     }
 }
