b62d9e2e3be6e193df3a71a2f28ada004ffbe815
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-05-21 19:08:48 +0200

    Add FailureSolution controller

---
+++ ReportingApp.UI/Controllers/SolutionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReportingApp.UI.Controllers
+{
+    public class SolutionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
