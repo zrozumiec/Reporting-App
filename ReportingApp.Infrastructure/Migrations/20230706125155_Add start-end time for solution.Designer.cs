f1179f6b144d5d520bd90c7900df8cae1a76f4b5
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-07-13 21:55:20 +0200

    Add migration start/end time solution

--- ReportingApp.Infrastructure/Migrations/20230706125155_Add start-end time for solution.Designer.cs
+++ ReportingApp.Infrastructure/Migrations/20230706125155_Add start-end time for solution.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReportingApp.Infrastructure;
 
@@ -11,9 +12,10 @@ using ReportingApp.Infrastructure;
 namespace ReportingApp.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230706125155_Add start-end time for solution")]
+    partial class Addstartendtimeforsolution
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ReportingApp.Infrastructure/Migrations/20230706125155_Add start-end time for solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportingApp.Infrastructure.Migrations
+{
+    public partial class Addstartendtimeforsolution : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ExpectedEndTime",
+                table: "FailureSolutions",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ExpectedStartTime",
+                table: "FailureSolutions",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpectedEndTime",
+                table: "FailureSolutions");
+
+            migrationBuilder.DropColumn(
+                name: "ExpectedStartTime",
+                table: "FailureSolutions");
+        }
+    }
+}
