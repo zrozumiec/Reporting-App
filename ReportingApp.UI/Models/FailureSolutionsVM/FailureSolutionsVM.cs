8d145f401c3a76b42608ebebfd127809c2ab4a00
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-08-13 21:46:43 +0200

    Add failure solution view-model

---
+++ ReportingApp.UI/Models/FailureSolutionsVM/FailureSolutionsVM.cs
@@ -0,0 +1,17 @@
+﻿using ReportingApp.Application.DTO;
+
+namespace ReportingApp.UI.Models.FailureSolutions
+{
+    public class FailureSolutionsVM
+    {
+        public string CurrentUserId { get; set; }
+
+        public bool AccesToAddSolution { get; set; }
+
+        public bool AccesToAcceptSolution { get; set; }
+
+        public bool AnyAccepted { get; set; }
+
+        public FailureDto Failure { get; set; } = new FailureDto();
+    }
+}
