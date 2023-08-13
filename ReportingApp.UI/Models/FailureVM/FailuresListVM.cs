7501279564ced63b402f647daba995a9905eec6b
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-08-13 21:45:55 +0200

    Add Failure collection view-model

---
+++ ReportingApp.UI/Models/FailureVM/FailuresListVM.cs
@@ -0,0 +1,11 @@
+﻿using ReportingApp.Application.DTO;
+
+namespace ReportingApp.UI.Models.FailureVM
+{
+    public class FailuresListVM
+    {
+        public ICollection<FailureDto> Failures { get; set; } = new List<FailureDto>();
+
+        public bool AccesToEdit { get; set; }
+    }
+}
