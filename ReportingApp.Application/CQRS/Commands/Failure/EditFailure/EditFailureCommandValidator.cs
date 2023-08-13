d15438e9ee5f3d339cb06fa1092d16f00bf7c938
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-08-13 21:41:04 +0200

    Add validator to command EditFailure

---
+++ ReportingApp.Application/CQRS/Commands/Failure/EditFailure/EditFailureCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+
+namespace ReportingApp.Application.CQRS.Commands.Failure.EditFailure
+{
+    public class EditFailureCommandValidator : AbstractValidator<EditFailureCommand>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditFailureCommandValidator"/> class.
+        /// </summary>
+        public EditFailureCommandValidator()
+        {
+            RuleFor(x => x.Failure.Name)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(150);
+
+            RuleFor(x => x.Failure.Location.City)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Failure.Location.Country)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Failure.Location.Factory)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Failure.Location.Machine)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Failure.Location.Street)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
