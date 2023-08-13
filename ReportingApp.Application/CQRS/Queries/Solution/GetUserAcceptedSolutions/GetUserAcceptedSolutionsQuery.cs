08be7a72d1b6e1ba154b8a28e9b41a43d349e9c1
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-08-13 21:39:24 +0200

    Add query GetUserAcceptedSolutions

---
+++ ReportingApp.Application/CQRS/Queries/Solution/GetUserAcceptedSolutions/GetUserAcceptedSolutionsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using ReportingApp.Application.DTO;
+
+namespace ReportingApp.Application.CQRS.Queries.Solution.GetAUserAcceptedSolutions
+{
+    public class GetUserAcceptedSolutionsQuery : IRequest<ICollection<FailureSolutionDto>>
+    {
+        public GetUserAcceptedSolutionsQuery(string userId)
+        {
+            UserId = userId;
+        }
+
+        public string UserId { get; }
+    }
+}
---
+++ ReportingApp.Application/CQRS/Queries/Solution/GetUserAcceptedSolutions/GetUserAcceptedSolutionsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using ReportingApp.Application.DTO;
+using ReportingApp.Domain.Interfaces;
+
+namespace ReportingApp.Application.CQRS.Queries.Solution.GetAUserAcceptedSolutions
+{
+    public class GetUserAcceptedSolutionsQueryHandler : IRequestHandler<GetUserAcceptedSolutionsQuery, ICollection<FailureSolutionDto>>
+    {
+        private readonly IFailureSolutionRepository repository;
+        private readonly IMapper mapper;
+
+        public GetUserAcceptedSolutionsQueryHandler(IFailureSolutionRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public async Task<ICollection<FailureSolutionDto>> Handle(GetUserAcceptedSolutionsQuery request, CancellationToken cancellationToken)
+        {
+            var acceptedSolutions = await this.repository.GetUserAcceptedFailureSolutionsAsync(request.UserId);
+
+            return this.mapper.Map<ICollection<FailureSolutionDto>>(acceptedSolutions);
+        }
+    }
+}
