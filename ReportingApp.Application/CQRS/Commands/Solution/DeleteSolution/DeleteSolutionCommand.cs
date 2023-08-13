4e8234ed28e2f3fa9fcba5f2348194aaa9e63f0a
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-08-13 21:40:28 +0200

    Add command DeleteSolution

---
+++ ReportingApp.Application/CQRS/Commands/Solution/DeleteSolution/DeleteSolutionCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace ReportingApp.Application.CQRS.Commands.Solution.DeleteSolution
+{
+    public class DeleteSolutionCommand : IRequest<int>
+    {
+        public DeleteSolutionCommand(int solutionId)
+        {
+            SolutionId = solutionId;
+        }
+
+        public int SolutionId { get; }
+    }
+}
---
+++ ReportingApp.Application/CQRS/Commands/Solution/DeleteSolution/DeleteSolutionCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using ReportingApp.Domain.Interfaces;
+
+namespace ReportingApp.Application.CQRS.Commands.Solution.DeleteSolution
+{
+    public class DeleteSolutionCommandHandler : IRequestHandler<DeleteSolutionCommand, int>
+    {
+        private readonly IFailureSolutionRepository repository;
+
+        public DeleteSolutionCommandHandler(IFailureSolutionRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<int> Handle(DeleteSolutionCommand request, CancellationToken cancellationToken)
+        {
+            return await this.repository.DeleteAsync(request.SolutionId);
+        }
+    }
+}
