db5a3827cf621325ae3f7b88d5787213d55699b6
Kamil Ciupak <kamil.ciupak@o2.pl>
2023-09-28 21:32:29 +0200

    Add action to send email

--- ReportingApp.UI/Controllers/HomeController.cs
+++ ReportingApp.UI/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using ReportingApp.Application.Email;
 using ReportingApp.UI.Models;
+using System.Diagnostics;
 
 namespace ReportingApp.UI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> logger;
+        private readonly IEmailService emailService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IEmailService emailService)
         {
             this.logger = logger;
+            this.emailService = emailService;
         }
 
         public IActionResult Index()
@@ -27,6 +30,22 @@ namespace ReportingApp.UI.Controllers
         public IActionResult Error()
         {
             return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SendEmail(EmailModel emailModel)
+        {
+            var result = await this.emailService.SendEmailAsync(emailModel);
+
+            if (result)
+            {
+                return this.StatusCode(StatusCodes.Status200OK, "OK");
+            }
+            else
+            {
+                return this.StatusCode(StatusCodes.Status400BadRequest);
+            }
         }
     }
 }
